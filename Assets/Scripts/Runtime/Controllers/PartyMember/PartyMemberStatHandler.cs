8878f662b86fbc9edb0f1ebc80fda77145b13b5b
Owner <genoimel@gmail.com>
2023-08-04 21:56:32 -0400

    Created PartyMonoSystem and scripted new party creation

--- Assets/Scripts/Runtime/Controllers/PartyMember/PartyMemberStatHandler.cs
+++ Assets/Scripts/Runtime/Controllers/PartyMember/PartyMemberStatHandler.cs
@@ -1,9 +1,48 @@
+using Akashic.ScriptableObjects.Scripts;
 using UnityEngine;
 
 namespace Akashic.Runtime.Controllers.PartyMember
 {
     internal sealed class PartyMemberStatHandler : MonoBehaviour
     {
+        [Header("Controller")]
+        [SerializeField] private PartyMemberController partyMemberController;
         
+        [Header("Stats")]
+        [SerializeField] public int currentLevel;
+        
+        [SerializeField] public int currentPhysicalAttack;
+        [SerializeField] public int currentMagicalAttack;
+        [SerializeField] public float currentAccuracy;
+        
+        [SerializeField] public int currentPhysicalDefense;
+        [SerializeField] public int currentMagicalDefense;
+        [SerializeField] public float currentEvade;
+        
+        private int basePhysicalAttack;
+        private int baseMagicalAttack;
+        private float baseAccuracy;
+        
+        private int basePhysicalDefense;
+        private int baseMagicalDefense;
+        private float baseEvade;
+        
+        public void InitializeNewPartyMemberFromScriptableObject(PartyMemberBaseData baseData)
+        {
+            basePhysicalAttack = baseData.basePhysicalAttack;
+            baseMagicalAttack = baseData.baseMagicalAttack;
+            baseAccuracy = baseData.baseAccuracy;
+
+            basePhysicalDefense = baseData.basePhysicalDefense;
+            baseMagicalDefense = baseData.baseMagicalDefense;
+            baseEvade = baseData.baseEvade;
+            
+            CalculateCurrentStats();
+        }
+
+        private void CalculateCurrentStats()
+        {
+            
+        }
     }
 }
---
+++ Assets/Scripts/Runtime/Controllers/SaveMenu/Messages.cs
@@ -0,0 +1,12 @@
+using Akashic.Core;
+
+namespace Akashic.Runtime.Controllers.SaveMenu
+{
+    internal sealed class ShowSaveMenuMessage : IMessage
+    {
+    }
+
+    internal sealed class HideSaveMenuMessage : IMessage
+    {
+    }
+}
