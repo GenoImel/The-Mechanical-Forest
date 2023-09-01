c8079f1c2a32ea4db1ced6ba40e7219638d80216
Jacob Krebs <jakek321@gmail.com>
2023-09-01 16:20:57 -0400

    Clean up dialogue test scene script

--- Assets/Scenes/Test/DialogueMonoSystem_Test/DialogueMonoSystemController_Test.cs
+++ Assets/Scenes/Test/DialogueMonoSystem_Test/DialogueMonoSystemController_Test.cs
@@ -1,21 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Akashic
 {
-    public class SEInjector_Controller_Test : MonoBehaviour
+    public class DialogueMonoSystemController_Test : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
