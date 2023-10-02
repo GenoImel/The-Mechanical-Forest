34a29abe319198361c464874f486e6899626606e
WhiteWolf93 <javiervelix@gmail.com>
2023-10-02 19:38:25 +0200

    first implementation of the Debugger MonoSystem

---
+++ Assets/Scripts/Runtime/MonoSystems/Debugger/DebuggerMonoSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Akashic.Runtime.MonoSystems.Debugger
+{
+    internal sealed class DebuggerMonoSystem : MonoBehaviour, IDebuggerMonoSystem
+    {
+    }
+}
