000ede8d950937f2b498a0e0d38ab2bc12c4d4ce
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-16 19:24:45 +0200

    Created scriptable objects ItemBase folder
    Created AccessoryBaseData ScriptableObject
    Created ItemBaseData ScriptableObject
    Created RelicBaseData ScriptableObject

---
+++ Assets/ScriptableObjects/ItemBase/ItemBaseData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Akashic.Assets.ScriptableObjects.ItemBase
+{
+	[CreateAssetMenu(menuName = "Akashic/Items/New Base Item")]
+	internal sealed class ItemBaseData : ScriptableObject
+	{
+		public string itemName;
+		public string description;
+		public Sprite icon;
+	}
+}
---
+++ Assets/ScriptableObjects/ItemBase/RelicBaseData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Akashic.Assets.ScriptableObjects.ItemBase
+{
+	[CreateAssetMenu(menuName = "Akashic/Items/New Base Relic")]
+	internal sealed class RelicBaseData : ScriptableObject
+	{
+		public string itemName;
+		public string description;
+		public Sprite icon;
+	}
+}
