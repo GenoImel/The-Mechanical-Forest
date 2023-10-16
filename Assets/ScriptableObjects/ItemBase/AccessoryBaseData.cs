05f7c11b62cd1e3fd4e4f21d8ac7f7dff2ae157a
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-16 20:43:22 +0200

    Create scriptable objects for items (#161)
    * Created scriptable objects ItemBase folder
    
    Created AccessoryBaseData ScriptableObject
    Created ItemBaseData ScriptableObject
    Created RelicBaseData ScriptableObject
    
    * Rename ItemBaseData to BattleItemBaseData

---
+++ Assets/ScriptableObjects/ItemBase/AccessoryBaseData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Akashic.Assets.ScriptableObjects.ItemBase
+{
+	[CreateAssetMenu(menuName = "Akashic/Items/New Base Accessory")]
+	internal sealed class AccessoryBaseData : ScriptableObject
+	{
+		public string itemName;
+		public string description;
+		public Sprite icon;
+	}
+}
---
+++ Assets/ScriptableObjects/ItemBase/BattleItemBaseData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Akashic.Assets.ScriptableObjects.ItemBase
+{
+	[CreateAssetMenu(menuName = "Akashic/Items/New Base Battle Item")]
+	internal sealed class BattleItemBaseData : ScriptableObject
+	{
+		public string itemName;
+		public string description;
+		public Sprite icon;
+	}
+}
