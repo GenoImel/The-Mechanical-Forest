8584bb3a65cd1c68ac4b1bc686999eddcc78f1af
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-18 18:45:46 +0200

    Updated ItemBase/AccessoryBaseData, ItemBase/RelicBaseData, ItemBase/BattleItemBaseData to include a itemId and itemDisplayName instead of itemName (#165)
    Updated all SOs instances to manage the new itemId and itemDisplayName fields
    
    itemId : name of item without spaces and capital letters
    itemDisplayName : name of item

--- Assets/ScriptableObjects/ItemBase/AccessoryBaseData.cs
+++ Assets/ScriptableObjects/ItemBase/AccessoryBaseData.cs
@@ -5,7 +5,8 @@ namespace Akashic.Assets.ScriptableObjects.ItemBase
 	[CreateAssetMenu(menuName = "Akashic/Items/New Base Accessory")]
 	internal sealed class AccessoryBaseData : ScriptableObject
 	{
-		public string itemName;
+		public string itemId;
+		public string itemDisplayName;
 		public string description;
 		public Sprite icon;
 	}
--- Assets/ScriptableObjects/ItemBase/BattleItemBaseData.cs
+++ Assets/ScriptableObjects/ItemBase/BattleItemBaseData.cs
@@ -5,7 +5,8 @@ namespace Akashic.Assets.ScriptableObjects.ItemBase
 	[CreateAssetMenu(menuName = "Akashic/Items/New Base Battle Item")]
 	internal sealed class BattleItemBaseData : ScriptableObject
 	{
-		public string itemName;
+		public string itemId;
+		public string itemDisplayName;
 		public string description;
 		public Sprite icon;
 	}
