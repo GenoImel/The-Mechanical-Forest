4ecf1d882cd5c6eb70511b012811756312980b27
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-23 20:12:31 +0200

    Added partyInventory save management to ISaveMonoSystem and SaveMonoSystem

--- Assets/Scripts/Runtime/MonoSystems/Save/ISaveMonoSystem.cs
+++ Assets/Scripts/Runtime/MonoSystems/Save/ISaveMonoSystem.cs
@@ -13,16 +13,21 @@ namespace Akashic.Runtime.MonoSystems.Save
         /// If it does, return the player defined save file name.
         /// </summary>
         public Task<string> FindSaveFile(string saveSlotFileName);
-        
-        /// <summary>
-        /// Returns the current party members as a list.
-        /// </summary>
-        public List<PartyMember> GetPartyMembers();
 
-        /// <summary>
-        /// Saves new data to the currently active save file.
-        /// </summary>
-        public void SaveFileAsync();
+		/// <summary>
+		/// Returns the current party members as a list.
+		/// </summary>
+		public List<PartyMember> GetPartyMembers();
+
+		/// <summary>
+		/// Returns the current inventory.
+		/// </summary>
+		public PartyInventory GetPartyInventory();
+
+		/// <summary>
+		/// Saves new data to the currently active save file.
+		/// </summary>
+		public void SaveFileAsync();
         
         /// <summary>
         /// Loads data from a selected save file.
