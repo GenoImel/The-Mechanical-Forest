5ec30f61c0c14753f67b1d3e82ea3f03a41d0d6b
GenoImel <genoimel@gmail.com>
2023-11-04 21:23:16 -0400

    Added methods and fields to DebugMonoSystem, created debug Party and Inventory

---
+++ Assets/Scripts/Runtime/MonoSystems/GameDebug/IDebugMonoSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Akashic.Core.MonoSystems;
+using Akashic.ScriptableObjects.Inventory;
+using Akashic.ScriptableObjects.PartyMember;
+
+namespace Akashic.Runtime.MonoSystems.GameDebug
+{
+    internal interface IDebugMonoSystem : IMonoSystem
+    {
+        /// <summary>
+        /// States whether or not debug mode is enabled for the game.
+        /// </summary>
+        public bool IsDebugMode { get; }
+        
+        /// <returns>A list of debug <see cref="PartyMemberData"/>.</returns>
+        public List<PartyMemberData> GetDebugParty();
+        
+        /// <returns>Returns the debug <see cref="InventoryData"/>.</returns>
+        public InventoryData GetDebugInventory();
+    }
+}
